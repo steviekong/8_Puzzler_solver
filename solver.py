9499f80cc5996e170fad59bc6055fe7f19344b51
steviekong <sidharth.reji@gmail.com>
2019-02-09 18:50:31 -0500

    Compeleted node and small fixes to text-array

--- solver.py
+++ solver.py
@@ -15,18 +15,26 @@ def heuristicSearch(startNode):
 	return 0
 
 class node:
-	def _init_(self, state, parent, depth, path_cost, children):
+	def __init__(self, state, parent, depth, path_cost, children):
 		self.state = state
 		self.parent = parent
 		self.depth = depth
 		self.path_cost = path_cost
-		self.chidren = chidren
+		self.children = children
 
 	def generate_child_nodes():
 		return 1
 
+	def find_zero(self):
+		for i, e in enumerate(self.state):
+			try:
+				return i, e.index(0)
+			except:
+				pass
+
+
 	def is_goal(self):
-		goal = [[0],[1],[2],[3],[4],[5],[6],[7],[8]]
+		goal = [[0, 1, 2],[3, 4, 5],[6, 7, 8]]
 		return self.state == goal
 
 def process_args():
@@ -48,22 +56,27 @@ def process_args():
 	else:
 		heuristicSearch()
 	'''
-	print(strategy_to_array(path))
+	print(strategy_to_array(path).find_zero())
 
 
 def strategy_to_array(path):
 	board = []
 	i = 0; 
+	j = 0;
 	with open(path, 'r') as openFile:
 			for line in openFile.read().splitlines():
+				board.append([])
 				for number in line.split(' '):
-					board.append([])
 					if number == '.':
-						board[i].append(0)
+						board[i].append(int(0))
+						j += 1
 					else: 
 						board[i].append(int(number))
-					i+=1
-	return board; 
+						j += 1
+					if j == 3:
+						j = 0
+						i += 1
+	return node(board, None, 0, 0, None)
 
 
 process_args();
