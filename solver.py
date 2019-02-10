01aa854ffb4636243d6c6ebec7877aab1ed5c601
steviekong <sidharth.reji@gmail.com>
2019-02-10 05:57:49 +0000

    Finished bfs and ucs

--- solver.py
+++ solver.py
@@ -1,29 +1,18 @@
 import numpy;
 import sys;
 from array import *; 
-
-
-def bfs(startNode):
- 	return 0
-
-def ucs(startNode):
-	return 0
-
-def gbf(startNode):
-	return 0
-def heuristicSearch(startNode):
-	return 0
+import copy; 
+from queue import *
+from heapq import *
 
 class node:
-	def __init__(self, state, parent, depth, path_cost, children):
+	def __init__(self, state, parent, depth, path_cost, children, self_cost):
 		self.state = state
 		self.parent = parent
 		self.depth = depth
 		self.path_cost = path_cost
 		self.children = children
-
-	def generate_child_nodes():
-		return 1
+		self.self_cost = self_cost
 
 	def find_zero(self):
 		for i, e in enumerate(self.state):
@@ -32,10 +21,135 @@ class node:
 			except:
 				pass
 
+	def left_move_child(self, i, j, child_list):
+			if(j-1 >= 0):
+				temp = copy.deepcopy(self.state)
+				temp[i][j-1], temp[i][j] = temp[i][j], temp[i][j-1]
+				child_list.append(node(temp, self, self.depth+1, self.path_cost+temp[i][j],None,temp[i][j])) 
+
+	def right_move_child(self, i, j, child_list):
+			if(j+1 <= 2):
+				temp = copy.deepcopy(self.state)
+				temp[i][j+1], temp[i][j] = temp[i][j], temp[i][j+1]
+				child_list.append(node(temp, self, self.depth+1, self.path_cost+temp[i][j],None,temp[i][j]))
+
+	def up_move_child(self, i, j, child_list):
+			if(i-1 >= 0):
+				temp = copy.deepcopy(self.state)
+				temp[i-1][j], temp[i][j] = temp[i][j], temp[i-1][j]
+				child_list.append(node(temp, self, self.depth+1, self.path_cost+temp[i][j],None,temp[i][j]))
+
+	def down_move_child(self, i, j, child_list):
+			if(i+1 <= 2):
+				temp = copy.deepcopy(self.state)
+				temp[i+1][j], temp[i][j] = temp[i][j], temp[i+1][j]
+				child_list.append(node(temp, self, self.depth+1, self.path_cost+temp[i][j],None,temp[i][j]))
+
+	def generate_child_nodes(self):
+		i, j = self.find_zero()
+
+		child_list = []
+
+		self.left_move_child(i, j, child_list)
+		self.right_move_child(i, j, child_list)
+		self.up_move_child(i, j, child_list)
+		self.down_move_child(i, j, child_list)
+		self.children = child_list
+
 
 	def is_goal(self):
 		goal = [[0, 1, 2],[3, 4, 5],[6, 7, 8]]
 		return self.state == goal
+
+def manhattanDistance(s_1, s_2):
+
+	return
+
+def gbf(start_node):
+	frontier = Queue()
+	explored = Queue()
+	frontier.put(start_node)
+	frontier_count = 0 
+	expanded_count = 0
+
+	while(not frontier.empty()):
+		node = frontier.get()
+		if node.is_goal():
+			return node, node.path_cost, frontier_count, expanded_count
+		explored.put(node)
+		node.generate_child_nodes()
+		expanded_count += 1
+		for n in node.children:
+			if n not in frontier.queue and n not in explored.queue :
+				frontier_count += 1
+				frontier.put(n)
+
+
+def ucs(start_node):
+	frontier = []
+	explored = Queue()
+	heappush(frontier, (0, id(start_node), start_node))
+	frontier_count = 0 
+	expanded_count = 0
+
+	while(frontier):
+		node = heappop(frontier)[2]
+		if node.is_goal():
+			return node, node.path_cost, frontier_count, expanded_count
+		explored.put(node)
+		node.generate_child_nodes()
+		expanded_count += 1
+		for n in node.children:
+			if not any(x[2].state == n.state for x in frontier) and not any(x.state == n.state for x in explored.queue):
+				heappush(frontier, (n.path_cost ,id(n), n))
+				frontier_count += 1
+
+def bfs(start_node):
+	frontier = Queue()
+	explored = Queue()
+	frontier.put(start_node)
+	frontier_count = 1 
+	expanded_count = 0
+
+	while(not frontier.empty()):
+		node = frontier.get()
+		explored.put(node)
+		node.generate_child_nodes()
+		expanded_count += 1
+		for n in node.children:
+			if not any(x.state == n.state for x in frontier.queue) and not any(x.state == n.state for x in explored.queue):
+				if n.is_goal():
+					return n, n.path_cost, frontier_count, expanded_count
+				else:
+					frontier_count += 1
+					frontier.put(n)
+def print_node(n):
+	for i in range(len(n.state)):
+		line = ""
+		for j in range(len(n.state[i])):
+			if n.state[i][j] is not 0:
+				line += str(n.state[i][j])+" "
+			else:
+				line += ". "
+		print(line)
+		line = ""
+	print("")
+
+def print_results(algorithm, start_node):
+	results = algorithm(start_node)
+	curr = results[0]
+	stack = []
+	while(curr is not None):
+		stack.append(curr)
+		curr = curr.parent
+
+	while(stack):
+		print_node(stack.pop())
+
+	print("path cost: "+str(results[1]))
+	print("frontier: "+str(results[2]))
+	print("expanded: "+str(results[3]))
+
 
 def process_args():
 	strategy = ''
@@ -43,20 +157,19 @@ def process_args():
 	try:
 		strategy = str(sys.argv[1])
 		path = str(sys.argv[2])
-		print (strategy + " " + path)
 	except:
 		print("Invalid Input: The correct way to run the program is \"python solver.py strategy path\"")
 
-	'''if strategy == 'bfs':
-		bfs()
+	if strategy == 'bfs':
+		print_results(bfs, strategy_to_array(path))
 	elif strategy == 'ucost':
-		ucs()
+		print_results(ucs, strategy_to_array(path))
+		'''
 	elif strategy == 'greedy':
 		gbf()
 	else:
 		heuristicSearch()
 	'''
-	print(strategy_to_array(path).find_zero())
 
 
 def strategy_to_array(path):
@@ -76,7 +189,7 @@ def strategy_to_array(path):
 					if j == 3:
 						j = 0
 						i += 1
-	return node(board, None, 0, 0, None)
+	return node(board, None, 0, 0, None, 0)
 
 
 process_args();
